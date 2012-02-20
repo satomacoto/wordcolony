348b2c0b10a7685c4ea7d79e8e66b3eba2bcdb18
Makoto Sato <sato@sato-air-3.local>
2012-02-21 00:55:45 +0900

    update some files

--- app.py
+++ app.py
@@ -2,14 +2,194 @@
 # -*- coding: utf-8 -*-
 import os
 
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
+
+
+import os
+import cgi
+import simplejson
+from urllib import urlencode, unquote_plus
+from xml.dom import minidom
+from src.parser import Parser
+from src.scene import analyze
+from src.dok_matrix import *
+from src.yahooapi import MAService
+import src.dok as dok
+
+def tokenize(text,lang='en'):
+    if lang == 'en':
+        return etokenize(text)
+    elif lang == 'ja':
+        if not isinstance(text, str):
+            text = text.encode('utf-8')
+        return jtokenize(text)
+
+def etokenize(text):
+    p = Parser()
+    sentences = []
+    for line in text.split('.'):
+        tokens = p.tokenise(line, stem=False)
+        if not tokens: continue
+        tokens = p.removeStopwords(tokens)
+        if not tokens: continue
+        sentences.append(tokens)
+    return sentences
+
+def jtokenize(text,sep=['.','。','．','\n'],filter=['9']):
+    # yahoo api
+    p = Parser()
+    client = MAService()
+
+    # set the end of sentence
+    eos = 'EOS'
+    
+    for x in sep:
+        text = text.replace(x, ' '+eos+' ')
+    text += eos
+    
+    # filter
+    filter = "|".join(filter)
+    
+    # get json
+    words = client.parse(text,filter=filter)
+    
+    # split document
+    sentences = []
+    temp = []
+    settoji = ""
+    for i in range(len(words)):
+        pos = words[i]['pos'].encode('utf-8')
+        baseform = words[i]['baseform'].encode('utf-8')
+        if baseform == eos:
+            sentences += [temp]
+            temp = []
+        # 接頭辞だったら一時的に保存
+        elif pos == '接頭辞':
+            settoji = baseform
+        # 接尾辞だったら前の語と一緒にする
+        elif pos == '接尾辞' and temp[-1]:
+            temp[-1] += baseform
+        else:
+            # 前が接頭辞だったら
+            if settoji:
+                baseform = settoji + baseform
+                settoji = ""
+            # ストップワーズ
+            if baseform in p.stopwords: continue
+            temp += [baseform]
+    
+    sentences = [ sentence for sentence in sentences if sentence ]
+#    sentences = [ [ word for word in sentence] for sentence in sentences ]
+    return sentences
+
+def get_scene(sentences):
+    tc,cc,td,attr = analyze(sentences)
+    return td,attr
+
+def get_bridge(source,target):
+    bridge = {}
+    return bridge
+    
+def to_kv(d):
+    r = []
+    for k,v in d.items():
+        r += [{ "key": k, "value": v }]
+    return r
+    
+def to_stv(d):
+    r = []
+    for (s,t),v in d.items():
+        if v > 0:
+            r += [{ "source": s, "target": t, "value": v }]
+    return r
+
+def output(handler,data,callback=''):
+    json = simplejson.dumps(data, ensure_ascii=False)
+    handler.response.headers["Content-Type"] = "text/javascript"
+    handler.response.out.write("%s(%s)" % (callback,json))
+
+def getxml(nodes, edges):
+    '''
+    nodes: { k:v, ...
+    edges: { (s,t):v, ...
+    '''
+    doc = minidom.Document()
+    
+    doc.appendChild(doc.createComment("This is a XML document"))
+    
+    # graphml
+    graphml = doc.createElement('graphml')
+    doc.appendChild(graphml)
+    
+    # key label    
+    def set_key(_id, _for, _attrname, _attrtype):
+        weight = doc.createElement('key')
+        for k,v in [('id',_id), ('for',_for), ('attr.name',_attrname), ('attr.type',_attrtype)]:
+            weight.setAttribute(k, v)
+        graphml.appendChild(weight)
+    set_key('label','all','label','string')
+    set_key('weight','node','weight','double')
+    
+    # graph
+    graph = doc.createElement('graph')
+    graph.setAttribute('edgedefault','directed')
+    graphml.appendChild(graph)
+    
+    def set_node(id, label, weight='1.0'):
+        node = doc.createElement('node')
+        node.setAttribute('id',id)
+        for k,v in [('label', label), ('weight', weight)]:
+            data = doc.createElement('data')
+            data.setAttribute('key', k)
+            data.appendChild(doc.createTextNode(v))
+            node.appendChild(data)
+        graph.appendChild(node)
+    
+    def set_edge(source, target, weight='1.0'):
+        edge = doc.createElement('edge')
+        edge.setAttribute('source',source)
+        edge.setAttribute('target',target)
+#        data = doc.createElement('data')
+#        data.setAttribute('key','weight')
+#        data.appendChild(doc.createTextNode(weight))
+#        edge.appendChild(data)
+        graph.appendChild(edge)
+    
+    # graph
+    for k,v in nodes.items():
+        set_node(k,k,str(v))
+    for (s,t),v in edges.items():
+        set_edge(s,t,str(v)) 
+    
+    return doc.toprettyxml(indent = '    ')
+
+@app.route('/td_attr', methods=['POST', 'GET'])
+def main():
+    text = request.form['text']
+    lang = request.form['lang'] if 'lang' in request.form else 'en'
+    output = request.form['output'] if 'output' in request.form else 'json'
+    callback = request.form['callback'] if 'callback' in request.form else ''
+    
+    #
+    tokens = tokenize(text, lang)
+    td, attr = get_scene(tokens)
+    if output == 'xml':
+        _td = {}
+        for (s,t),v in td.items():
+            if s <> t and v > 0.8:
+                _td[s,t] = v
+        return getxml(attr, _td)
+    else:
+        scene = {"td": to_stv(td), "attr": to_kv(attr)}
+        json = simplejson.dumps({ "elements":scene }, ensure_ascii=False)
+        return "%s(%s)" % (callback, json.encode('utf-8'))
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template('d3js.html')
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
